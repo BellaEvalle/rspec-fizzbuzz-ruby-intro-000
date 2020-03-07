8195634d0e3fdf286f38a0e24f54aa4dd5842fb9
Bella Evalle <bellaevalle@yahoo.com>
2020-03-07 22:57:06 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz (number)
-  if (number / 3 == 0) && (number /5 == 0)
+def fizzbuzz(number)
+  if (number % 3 == 0) && (number % 5 == 0)
     puts "FizzBuzz"
-    elseif number / 5 == 0
+    elseif number % 5 == 0
     puts "Buzz"
-  elseif number / 3 == 0
+  elseif number % 3 == 0
   puts "Fizz"
   end
 end

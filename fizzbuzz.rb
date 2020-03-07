f0e9d22ee8297a6578f42e2db9149ee7693c989e
Bella Evalle <bellaevalle@yahoo.com>
2020-03-07 22:56:15 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@
 def fizzbuzz (number)
   if (number / 3 == 0) && (number /5 == 0)
     puts "FizzBuzz"
+    elseif number / 5 == 0
+    puts "Buzz"
   elseif number / 3 == 0
   puts "Fizz"
-elseif number / 5 == 0
-puts "Buzz"
   end
-end   
+end
